1a4918a3626c47957df808a0d45756debbad8180
Developer Dan <developer@agilai.com>
2026-03-11 23:57:38 +0000

    feat(SCRUM-279): Implement decrement button

--- script.js
+++ script.js
@@ -3,8 +3,14 @@
 let counter = 0;
 const counterDisplay = document.getElementById('counter-display');
 const incrementBtn = document.getElementById('increment-btn');
+const decrementBtn = document.getElementById('decrement-btn');
 
 incrementBtn.addEventListener('click', () => {
     counter++;
+    counterDisplay.textContent = counter;
+});
+
+decrementBtn.addEventListener('click', () => {
+    counter--;
     counterDisplay.textContent = counter;
 });
